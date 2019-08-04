7f0f5fa7fbd66aa8cfbc8bc02861b2dfec366c25
Jayden Shamhu <jayden@siege.ai>
2019-08-04 10:48:42 +0200

    added functions for CRUD FailedSettlement objects

--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
@@ -9,14 +9,25 @@ package io.pleo.antaeus.data
 
 import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.Customer
+import io.pleo.antaeus.models.FailedSettlement
+import io.pleo.antaeus.models.FailedSettlementReason
+import io.pleo.antaeus.models.FailedSettlementStatus
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
+
+import java.time.Instant
+
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.SqlExpressionBuilder
+import org.jetbrains.exposed.sql.update
 import org.jetbrains.exposed.sql.transactions.transaction
+
+private const val MAX_RETRIES = 3
 
 class AntaeusDal(private val db: Database) {
     fun fetchInvoice(id: Int): Invoice? {
@@ -79,5 +90,46 @@ class AntaeusDal(private val db: Database) {
         }
 
         return fetchCustomer(id!!)
+    }
+
+    fun createFailedSettlement(invoice: Invoice, reasonCreated: FailedSettlementReason, log: String): Int? {
+        val id = transaction(db) {
+            // creates a new charge attempt item and returns its new id.
+            FailedSettlementTable
+                .insert {
+                    it[this.invoiceId] = invoice.id
+                    it[this.reasonCreated] = reasonCreated.toString()
+                    it[this.log] = log
+                    it[this.dateCreated] = Instant.now().getEpochSecond()
+                } get FailedSettlementTable.id
+        }
+        return id!!
+    }
+
+    fun updateFailedSettlement(failedSettlement: FailedSettlement, log: String?, status: FailedSettlementStatus?): Int {
+        transaction(db) {
+            FailedSettlementTable.update({ FailedSettlementTable.id eq failedSettlement.id }) {
+                with(SqlExpressionBuilder) {
+                    it.update(FailedSettlementTable.retries, FailedSettlementTable.retries + 1)
+                }
+            }
+        }
+        val id = transaction(db) {
+            // updates a failed settlement depending on the parameters passed
+            FailedSettlementTable.update({ FailedSettlementTable.id eq failedSettlement.id }) {
+                it[this.status] = status.toString()
+                it[this.log] = log.toString()
+            }
+        }
+        return id!!
+    }
+
+    fun fetchFailedSettlements(): List<FailedSettlement> {
+        // will select all charge attempt which have not exceeded MAX_RETRIES == 3
+        return transaction(db) {
+            FailedSettlementTable
+                .select { (FailedSettlementTable.retries less MAX_RETRIES) and (FailedSettlementTable.status eq FailedSettlementStatus.UNRESOLVED.toString()) }
+                .map { it.toFailedSettlement() }
+        }
     }
 }
