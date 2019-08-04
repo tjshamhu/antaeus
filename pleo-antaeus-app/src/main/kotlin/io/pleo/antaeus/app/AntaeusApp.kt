a047641b3a036c1bf83e65c61e1826f84b732ddb
Jayden Shamhu <jayden@siege.ai>
2019-08-04 12:08:17 +0200

    added billing service class and methods

--- pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
+++ pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
@@ -57,7 +57,7 @@ fun main() {
     val customerService = CustomerService(dal = dal)
 
     // This is _your_ billing service to be included where you see fit
-    val billingService = BillingService(paymentProvider = paymentProvider)
+    val billingService = BillingService(dal = dal, paymentProvider = paymentProvider)
 
     // Create REST web service
     AntaeusRest(
--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,88 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.FailedSettlement
+import io.pleo.antaeus.models.FailedSettlementReason
+import io.pleo.antaeus.models.FailedSettlementStatus
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
-class BillingService(
-    private val paymentProvider: PaymentProvider
+import mu.KotlinLogging
+
+
+private val logger = KotlinLogging.logger {}
+
+class BillingService (
+    private val paymentProvider: PaymentProvider,
+    private val dal: AntaeusDal
 ) {
-   // TODO - Add code e.g. here
+
+    fun settleUpaidInvoices(): Unit {
+       val unpaidInvoices = dal.fetchUpdaidInvoices()
+       for (invoice in unpaidInvoices) {
+           settleInvoice(invoice)
+       }
+    }
+
+    fun settleInvoice(invoice: Invoice): Boolean {
+        try {
+                val charged = paymentProvider.charge(invoice)
+                if (charged) {
+                    logger.info { "Invoice [${invoice.id}] successfully charged." }
+                    // notify customer, email billing, 
+                    dal.updateInvoice(invoice, InvoiceStatus.PAID)
+                } else {
+                    // notify customer not enough balance
+                    logger.info { "Invoice [${invoice.id}] failed to charge because [INSUFFICIENT_FUNDS]." }
+                    dal.createFailedSettlement(
+                        invoice = invoice, 
+                        reasonCreated = FailedSettlementReason.INSUFFICIENT_FUNDS, 
+                        log = "Insufficent funds to debit account"
+                    )
+                    logger.info { "Invoice [${invoice.id}] marked for reconciliation." }
+                }
+                return charged
+            } catch (e: CurrencyMismatchException) {
+                // log this, email customer, notify devs
+                logger.info { "Invoice [${invoice.id}] failed to because [CURRENCY_MISMATCH]. [${e.toString()}]" }
+                dal.createFailedSettlement(
+                    invoice = invoice, 
+                    reasonCreated = FailedSettlementReason.CURRENCY_MISMATCH, 
+                    log = e.toString()
+                )
+                logger.info { "Invoice [${invoice.id}] marked for reconciliation." }
+                return false
+
+            } catch (e: CustomerNotFoundException) {
+                // notify devs
+                logger.info { "Invoice [${invoice.id}] Customer [${invoice.customerId}] not found." }
+                return false
+
+            } catch (e: NetworkException) {
+                // log this, notify devs
+                logger.info { "Invoice [${invoice.id}] failed to because [NETWORK_ERROR]. [${e.toString()}]" }
+                dal.createFailedSettlement(
+                    invoice = invoice, 
+                    reasonCreated = FailedSettlementReason.NETWORK_ERROR, 
+                    log = e.toString()
+                )
+                logger.info { "Invoice [${invoice.id}] marked for reconciliation." }
+                return false
+
+            } catch (e: InvoiceNotFoundException) {
+                // log this, notify devs
+                logger.info { "Invoice [${invoice.id}] not found. [${e.toString()}]" }
+                return false
+
+            } catch (e: Exception) {
+                // log this,  notify devs
+                logger.info { "General excpetion occured with Invoice [${invoice.id}]. [${e.toString()}]." }
+                return false
+            }
+    }
 }
