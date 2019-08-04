6333850b98954c738d96c0babc7bf24e7f8a5b44
Jayden Shamhu <jayden@siege.ai>
2019-08-04 10:06:12 +0200

    added mapping function for FailedSettlement objects

--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/mappings.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/mappings.kt
@@ -7,6 +7,9 @@ package io.pleo.antaeus.data
 
 import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.Customer
+import io.pleo.antaeus.models.FailedSettlement
+import io.pleo.antaeus.models.FailedSettlementStatus
+import io.pleo.antaeus.models.FailedSettlementReason
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
@@ -25,4 +28,14 @@ fun ResultRow.toInvoice(): Invoice = Invoice(
 fun ResultRow.toCustomer(): Customer = Customer(
     id = this[CustomerTable.id],
     currency = Currency.valueOf(this[CustomerTable.currency])
+)
+
+fun ResultRow.toFailedSettlement(): FailedSettlement = FailedSettlement(
+    id = this[FailedSettlementTable.id],
+    invoiceId = this[FailedSettlementTable.invoiceId],
+    status = FailedSettlementStatus.valueOf(this[FailedSettlementTable.status]),
+    dateCreated = this[FailedSettlementTable.dateCreated],
+    reasonCreated = FailedSettlementReason.valueOf(this[FailedSettlementTable.reasonCreated]),
+    log = this[FailedSettlementTable.log],
+    retries = this[FailedSettlementTable.retries]
 )
